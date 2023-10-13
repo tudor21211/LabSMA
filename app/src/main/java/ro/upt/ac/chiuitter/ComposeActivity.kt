dda9722ea21357aea96a850a3a79d9902973bfb9
Claudiu Groza <claudiugroza@admins-MacBook-Pro.local>
2023-10-13 09:53:58 +0300

    Add compose and migrate layouts to composables

--- app/src/main/java/ro/upt/ac/chiuitter/ComposeActivity.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/ComposeActivity.kt
@@ -3,8 +3,30 @@ package ro.upt.ac.chiuitter
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.view_compose.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 
 // TODO 4: All application components must be added to manifest. Make sure to add this one.
@@ -12,21 +34,57 @@ class ComposeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_compose)
 
-        fab_finish.setOnClickListener {
-            // Prepare the intent to be returned as result.
-            val returnIntent = Intent().apply {
-                val content = etx_content.editableText.toString()
-                putExtra(EXTRA_TEXT, content)
+        setContent { ComposeScreen() }
+    }
+
+    @Composable
+    private fun ComposeScreen() {
+        var text by remember { mutableStateOf("") }
+
+        Surface(color = Color.White) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                TextField(
+                    modifier = Modifier
+                        .align(Alignment.TopCenter)
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text(stringResource(R.string.add_your_new_chiuit)) }
+                )
+                FloatingActionButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(16.dp),
+                    onClick = { onEditClick(text) },
+                ) {
+                    Icon(
+                        Icons.Filled.Check,
+                        stringResource(R.string.check_action_icon_content_description)
+                    )
+                }
             }
-
-            // Mark the result as success and attach text data.
-            setResult(Activity.RESULT_OK, returnIntent)
-
-            // Finish the activity, we do not need it anymore after we got the text input.
-            finish()
         }
+    }
+
+    private fun onEditClick(text: String) {
+        // Prepare the intent to be returned as result.
+        val returnIntent = Intent().apply {
+            putExtra(EXTRA_TEXT, text)
+        }
+
+        // Mark the result as success and attach text data.
+        setResult(Activity.RESULT_OK, returnIntent)
+
+        // Finish the activity, we do not need it anymore after we got the text input.
+        finish()
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun DefaultPreview() {
+        ComposeScreen()
     }
 
     companion object {
--- app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
@@ -1,19 +1,87 @@
 package ro.upt.ac.chiuitter
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.view_home.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class HomeActivity : AppCompatActivity() {
 
+    private val chiuitText = mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_home)
+        chiuitText.value = getText(R.string.chiuit_example).toString()
+        setContent { HomeScreen() }
+    }
 
-        ibt_share.setOnClickListener { shareChiuit(txv_content.text.toString()) }
-        fab_add.setOnClickListener { composeChiuit() }
+    @Composable
+    private fun HomeScreen() {
+        val text by remember { chiuitText }
+
+        Surface(color = Color.White) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            modifier = Modifier
+                                .weight(0.8f)
+                                .padding(8.dp),
+                            text = text,
+                        )
+                        Button(
+                            modifier = Modifier
+                                .weight(0.2f)
+                                .padding(8.dp),
+                            onClick = { shareChiuit(text) }) {
+                            Icon(
+                                Icons.Filled.Send,
+                                stringResource(R.string.send_action_icon_content_description)
+                            )
+                        }
+                    }
+                }
+                FloatingActionButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(16.dp),
+                    onClick = { composeChiuit() },
+                ) {
+                    Icon(
+                        Icons.Filled.Edit,
+                        stringResource(R.string.edit_action_icon_content_description)
+                    )
+                }
+            }
+        }
     }
 
     /*
@@ -47,7 +115,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            COMPOSE_REQUEST_CODE -> if (resultCode == Activity.RESULT_OK) extractText(data)
+            COMPOSE_REQUEST_CODE -> if (resultCode == RESULT_OK) extractText(data)
             else -> super.onActivityResult(requestCode, resultCode, data)
         }
     }
@@ -57,10 +125,15 @@ class HomeActivity : AppCompatActivity() {
             // TODO 5: Extract the text from result intent.
 
 
-            // TODO 6: Check if text is not null or empty, then set the new "chiuit" content.
-
+            // TODO 6: Check if text is not null or empty, then set the new "chiuitText".
 
         }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun DefaultPreview() {
+        HomeScreen()
     }
 
     companion object {
